c2ba7ba212a35735421682b37949b58d49facf19
Vishwanath777 <vishwak927@gmail,com>
2023-10-11 22:33:09 +0530

    Full code

---
+++ src/resumeData.js
@@ -0,0 +1,103 @@
+let resumeData = {
+    "imagebaseurl":"https://rbhatia46.github.io/",
+    "name": "Vishwanath P",
+    "role": "Frontend Developer",
+    "linkedinId":"Your LinkedIn Id",
+    "skypeid": "Your skypeid",
+    "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://in.linkedin.com/in/vishwanath-p-a94663185",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/Vishwanath777",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"skype",
+          "url":"https://join.skype.com/invite/sG7654heG2Yn",
+          "className":"fa fa-skype"
+        }
+      ],
+    "aboutme":"A results-driven Front-End Developer with five years of experience, specializing in UI design, web development, and React JS. A proven track record of collaborating cross-functionally with technical teams and stakeholders across all phases of the software development lifecycle. Adept at performing quality assurance testing for web applications.",
+    "address":"Kundapura, Udupi Dist, Karnataka 576283",
+    "website":"vishwak927@gmail.com",
+    "phoneNumber": "9535665329",
+    "education":[
+      {
+        "UniversityName":"Shree Dharmasthala Manjunatheshwara College Ujire",
+        "specialization":"BCA",
+        "MonthOfPassing":"June",
+        "YearOfPassing":"2017",
+        "Achievements":"Some Achievements"
+      },
+      {
+        "UniversityName":"RNS PU College, Kundapura",
+        "specialization":"PCMC",
+        "MonthOfPassing":"April",
+        "YearOfPassing":"2015",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Inboundsys Webgalaxy Pvt Ltd",
+        "specialization":"Frontend Developer",
+        "MonthOfLeaving":"March 2018",
+        "YearOfLeaving":"Present",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS3"
+      },
+      {
+        "skillname":"Reactjs"
+      },
+      {
+        "skillname":"JavaScript"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData
